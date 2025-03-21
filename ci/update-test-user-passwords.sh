d6dd7915ca030a8cfa4bf4bf559fed97b63cff84
Mark Boyd <mark.boyd@gsa.gov>
2025-03-21 09:51:45 -0400

    fix credhub var references in script to deploy-logs-opensearch pipeline

--- ci/update-test-user-passwords.sh
+++ ci/update-test-user-passwords.sh
@@ -12,7 +12,7 @@ for credential_name in $TEST_USER_CREDENTIAL_NAMES; do
   printf "updating password credential for %s\n\n" "$credential_name"
 
   # Generate a new password for the credential
-  credhub regenerate -n "/concourse/main/opensearch-dashboards-cf-auth-proxy/$credential_name"
+  credhub regenerate -n "/concourse/main/deploy-logs-opensearch/$credential_name"
 
   # Get the UAA username for the corresponding Credhub credential
   USERNAME=$(echo "$TEST_USERS_CREDENTIAL_USERNAME_MAP" | jq -r --arg credential_name "$credential_name" '.[$credential_name]')
@@ -20,7 +20,7 @@ for credential_name in $TEST_USER_CREDENTIAL_NAMES; do
   printf "updating UAA password for %s\n\n" "$USERNAME"
 
   # Get the new password from Credhub
-  PASSWORD=$(credhub get -n "/concourse/main/opensearch-dashboards-cf-auth-proxy/$credential_name" --output-json | jq -r '.value')
+  PASSWORD=$(credhub get -n "/concourse/main/deploy-logs-opensearch/$credential_name" --output-json | jq -r '.value')
 
   # Update the user password in UAA with the new value from Credhub
   uaac password set "$USERNAME" --password "$PASSWORD"
