0035e8858faae24d5c9052de2163533889dad692
Mark Boyd <mark.boyd@gsa.gov>
2025-04-09 17:32:22 -0400

    update tracing mode for e2e tests

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -4,4 +4,4 @@ python -m venv venv
 source venv/bin/activate
 pip install -r ci/requirements.txt
 playwright install firefox
-python -m pytest e2e --browser firefox --tracing on "$@"
+python -m pytest e2e --browser firefox --tracing retain-on-failure "$@"
