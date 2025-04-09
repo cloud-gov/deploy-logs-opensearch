7f877f60e4d8ce6442fa48bf35dd2b47cca3da46
Mark Boyd <mark.boyd@gsa.gov>
2025-04-09 17:09:24 -0400

    update CI script to create test users if they do not already exist

--- ci/update-test-user-passwords.sh
+++ ci/update-test-user-passwords.sh
@@ -14,18 +14,28 @@ for credential_name in $TEST_USER_CREDENTIAL_NAMES; do
   printf "updating password credential for %s\n\n" "$credential_name"
 
   # Generate a new password for the credential
-  credhub regenerate -n "/concourse/main/deploy-logs-opensearch/$credential_name"
-
+  PASSWORD_CREDENTIAL="/concourse/main/deploy-logs-opensearch/$credential_name"
+  if ! credhub get -n "$PASSWORD_CREDENTIAL"; then
+    credhub generate -n "$PASSWORD_CREDENTIAL" --type password
+  else 
+    credhub regenerate -n "$PASSWORD_CREDENTIAL"
+  fi
+  
   # Get the UAA username for the corresponding Credhub credential
   USERNAME=$(echo "$TEST_USERS_CREDENTIAL_USERNAME_MAP" | jq -r --arg credential_name "$credential_name" '.[$credential_name]')
 
-  printf "updating UAA password for %s\n\n" "$USERNAME"
-
   # Get the new password from Credhub
-  PASSWORD=$(credhub get -n "/concourse/main/deploy-logs-opensearch/$credential_name" --output-json | jq -r '.value')
+  PASSWORD=$(credhub get -n "$PASSWORD_CREDENTIAL" --output-json | jq -r '.value')
 
-  # Update the user password in UAA with the new value from Credhub
-  uaac password set "$USERNAME" --password "$PASSWORD"
+  if ! uaac user get "$USERNAME"; then
+    printf "Creating UAA user %s\n\n" "$USERNAME"
+    uaac user add "$USERNAME" --password "$PASSWORD" --origin cloud.gov --emails "$USERNAME"
+  else
+    printf "updating UAA password for %s\n\n" "$USERNAME"
+
+    # Update the user password in UAA with the new value from Credhub
+    uaac password set "$USERNAME" --password "$PASSWORD"  
+  fi
 
   # Activate the user, just to be safe
   uaac user activate "$USERNAME"
