5acbe6d7279a55c737cca147cd78165a3eaa0e86
Mark Boyd <mark.boyd@gsa.gov>
2025-04-10 10:27:25 -0400

    update login smoke tests to assert arriving at the homepage

--- e2e/test_smoke_tests_login.py
+++ e2e/test_smoke_tests_login.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-import os
+import re
 import logging
+from playwright.sync_api import expect
+from urllib.parse import urljoin
 
 from .utils import log_in
 from . import AUTH_PROXY_URL
@@ -11,3 +13,4 @@ logging.basicConfig(level=logging.DEBUG)
 
 def test_user_login(user_1, page):
     log_in(user_1, page, AUTH_PROXY_URL)
+    expect(page).to_have_url(re.compile(f"{urljoin(AUTH_PROXY_URL, 'app/home')}.*"))
