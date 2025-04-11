ef191c86bc67a06168557e61f70e2694c33a32f3
Mark Boyd <mark.boyd@gsa.gov>
2025-04-11 11:13:43 -0400

    start scaffolding test to create notification objects

--- e2e/test_alerting_access.py
+++ e2e/test_alerting_access.py
@@ -7,3 +7,13 @@ from . import AUTH_PROXY_URL, CF_ORG_1_NAME
 def test_user_can_create_alerts(user_1, page):
     log_in(user_1, page, AUTH_PROXY_URL)
     switch_tenants(page, CF_ORG_1_NAME)
+
+    # open the hamburger menu
+    hamburger_button = page.get_by_label("Toggle primary navigation")
+    hamburger_button.wait_for()
+    hamburger_button.click()
+
+    # go to the notifications page
+    notifications_menu_link = page.get_by_text("Notifications")
+    notifications_menu_link.wait_for()
+    notifications_menu_link.click()
