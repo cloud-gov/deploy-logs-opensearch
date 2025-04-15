e625ca6dbd8ef7c1e246690f43548b3e37172c48
Mark Boyd <mark.boyd@gsa.gov>
2025-04-15 11:59:05 -0400

    Add E2E tests for alerting access (#156)
    * update CI script to create test users if they do not already exist
    
    * move playwright tests to e2e folder
    
    * update test setup
    
    * update e2e-local.sh script to run Playwright tests in headed mode
    
    * update tracing mode for e2e tests
    
    * update pipeline vars for e2e tests
    
    * update pipeline vars for e2e tests
    
    * add other test user fixtures for playwright tests
    
    * add env vars for test users to dev tests
    
    * update branch temporarily
    
    * update script to ignore output for commands in if conditions
    
    * temporarily remove triggers for smoke tests login job in development
    
    * rename job from smoke-tests-login-development to e2e-tests-development
    
    * fix last reference from smoke-tests-login-development to e2e-tests-development
    
    * rename smoke-tests-login -> e2e-tests
    
    * rename smoke-tests-login -> e2e-tests
    
    * remove unused pipeline vars
    
    * update login smoke tests to assert arriving at the homepage
    
    * add pipeline job to provision test user CF access
    
    * add general-task input to e2e tests job in development
    
    * add vars for CF login to e2e tests development job
    
    * add vars for CF login to e2e tests development job
    
    * update script for provisioning test users to add them as org users
    
    * remove failure notification
    
    * update script to exit immediately on error
    
    * update function for setting users as org users
    
    * update script to set other test users as org users
    
    * make more env vars required for e2e tests
    
    * add switch_tenants helper for e2e tests
    
    * add env vars for CF orgs to e2e tests job in pipeline
    
    * add e2e tests for creating alerts
    
    * update e2e local script to pass along arguments
    
    * fix arguments to switch_tenants
    
    * cleanup test code
    
    * update logic for switching tenants in e2e tests
    
    * start scaffolding test to create notification objects
    
    * add e2e test for user to create email recipient group
    
    * add e2e tests for creating notifications channel
    
    * remove unused code
    
    * add e2e test to delete notifications channel
    
    * add e2e test for deleting channels
    
    * add e2e test for deleting email recipient groups
    
    * scaffold e2e test for creating monitors
    
    * fixing locators for e2e test of creating monitor
    
    * finish e2e test for creating an alert monitor
    
    * refactor e2e tests to use shared helpers
    
    * remove commented code
    
    * update e2e tests to use assertions and update CSS-based selector to select based on surrounding markup content
    
    * move shared e2e test helpers to utils.py
    
    * finish e2e test verifying that user cannot see notification objects
    
    * update e2e test for deleting objects to include deleting alert monitor
    
    * add e2e test for case where user can see but not edit objects
    
    * replace wait_for_loadstate("networkidle") with element assertion for loading indicator
    
    * update tests to use wait_for_loading_finished helper to detect when page loading is finished
    
    * update helper for switching tenants
    
    * refactor e2e test helpers
    
    * re-enable deletion tests
    
    * re-enable on failure notification
    
    * re-enable e2e job triggers
    
    * add pipeline steps to provision CF access for e2e test users

---
+++ ci/provision-test-user-cf-access.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! cf target > /dev/null; then
+  cf api "${CF_API_URL}"
+  cf auth
+fi
+
+required_env_vars=(
+  TEST_USER_1_USERNAME
+  TEST_USER_2_USERNAME
+  TEST_USER_3_USERNAME
+  TEST_USER_4_USERNAME
+  CF_ORG_1_NAME
+  CF_ORG_2_NAME
+  CF_ORG_3_NAME
+)
+for var in "${required_env_vars[@]}"; do
+  if [ -z "${!var+x}" ]; then
+    echo "$var is a required environment variable"
+    exit 1
+  fi
+done
+
+function set_org_user() {
+  USERNAME_ESCAPED=$(echo "$1" | jq -Rr @uri)
+  USER_GUID=$(cf curl "/v3/users?partial_usernames=$USERNAME_ESCAPED" | jq -er '.resources[0].guid')
+  ORG_GUID=$(cf org "$2" --guid)
+  TMP_FILE=$(mktemp)
+
+  cat > "${TMP_FILE}" << EOF
+{
+  "type": "organization_user",
+  "relationships": {
+    "user": {
+      "data": {
+        "guid": "$USER_GUID"
+      }
+    },
+    "organization": {
+      "data": {
+        "guid": "$ORG_GUID"
+      }
+    }
+  }
+}
+EOF
+
+  cf curl "/v3/roles" \
+    -X POST \
+    -d "@$TMP_FILE"
+  
+  rm "$TMP_FILE"
+}
+
+# Expected results:
+#  - User 1 is in org 1 and org 3
+#  - User 2 is in org 2. User 2 shares no orgs with User 1.
+#  - User 3 is in org 1. User 3 shares 1 org with User 1.
+#  - User 4 is in org 1 and org 3. User 4 shares all orgs with User 1.
+
+cf create-org "$CF_ORG_1_NAME"
+cf create-org "$CF_ORG_2_NAME"
+cf create-org "$CF_ORG_3_NAME"
+
+# User 1 is an org manager in org 1 and org 3
+cf set-org-role "$TEST_USER_1_USERNAME" "$CF_ORG_1_NAME" OrgManager
+set_org_user "$TEST_USER_1_USERNAME" "$CF_ORG_1_NAME"
+cf set-org-role "$TEST_USER_1_USERNAME" "$CF_ORG_3_NAME" OrgManager
+set_org_user "$TEST_USER_1_USERNAME" "$CF_ORG_3_NAME"
+
+# User 2 is an org manager in org 2
+cf set-org-role "$TEST_USER_2_USERNAME" "$CF_ORG_2_NAME" OrgManager
+set_org_user "$TEST_USER_2_USERNAME" "$CF_ORG_2_NAME"
+
+# User 3 is an org manager in org 1
+cf set-org-role "$TEST_USER_3_USERNAME" "$CF_ORG_1_NAME" OrgManager
+set_org_user "$TEST_USER_3_USERNAME" "$CF_ORG_1_NAME"
+
+# User 4 is an org manager in org 1 and org 3
+cf set-org-role "$TEST_USER_4_USERNAME" "$CF_ORG_1_NAME" OrgManager
+set_org_user "$TEST_USER_4_USERNAME" "$CF_ORG_1_NAME"
+cf set-org-role "$TEST_USER_4_USERNAME" "$CF_ORG_3_NAME" OrgManager
+set_org_user "$TEST_USER_4_USERNAME" "$CF_ORG_3_NAME"
+
