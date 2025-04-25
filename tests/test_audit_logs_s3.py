778b6f1f875a2a1930ec7200671832e73292f1e0
Jason Gambino <jason.gambino3@gmail.com>
2025-04-25 15:42:11 -0400

    Audit s3 job and supporting tasks addition (#166)
    * testing out a task to do s3
    
    * adding pipeline.yml
    
    * adding in a ls to see if its there
    
    * adding in a ls to see if its there
    
    * more tweaking
    
    * boto3 needed
    
    * maybe missing params for aws?
    
    * explicit saying
    
    * adding in better logging/complete logs for audit object and cf_login
    
    * adding in the addition of multiple pages,formatting for logstash and cf pulls
    
    * making bucket pull from credhub
    
    * fixing typo
    
    * putting in functools
    
    * putting in api
    
    * putting in api
    
    * trying to login to cf
    
    * removing api call
    
    * adding cf client
    
    * attempt at cf
    
    * changing cf permissions
    
    * changing cf permissions
    
    * removing from yml
    
    * aggressive cf login script
    
    * seeing if it prints right
    
    * trying different privledges
    
    * forgot hour
    
    * python vs python3 check
    
    * adding in missing pieces for it not to error
    
    * page was not going in right
    
    * last fix
    
    * missing page
    
    * adding script for pulling s3 value
    
    * changing branch back
    
    * changing back to audits3
    
    * figuring out where the nonetype comes from
    
    * giving error times
    
    * need to error if error
    
    * changing back to main
    
    * updating script to work for logs without orgs or spaces
    
    * branch change
    
    * seperation of audit to different environments and the job for the ingestor
    
    * putting it in to structured catagory
    
    * adding in missing credhub values
    
    * temporary to get them started hot
    
    * storing in good state
    
    * changing to work with empty list
    
    * updating to allow dev and staging to have a starting point
    
    * adding back in timestamp
    
    * adding back in timestamp
    
    * adding back in timestamp
    
    * better timer handling
    
    * adding missing piece of audit
    
    * putting in the bucket
    
    * renaming credhub value
    
    * cleanup
    
    * adding in more consumes
    
    * missing passing of opensearch certs
    
    * remove failure message
    
    * adding back in failures
    
    * changing back to main
    
    * like minute added better
    
    * changing branch back
    
    * removing again
    
    * removing line
    
    * adding missing variables
    
    * Audit s3 python refactor (#167)
    
    * refactor python script for writing audit logs to S3:
    
      - remove use of subprocesses and replace with calls to request
      - move exception handing to top level
      - handle case where timestamp for audit logs has not yet been created
      - move logic for transforming audit event records into separate function
    
    * fix arguments to get_cf_entity_name
    
    * remove change to end_time when start_time is read from S3
    
    * changing token to api
    
    * changing response to use uaa api
    
    * realized missing alot
    
    * realized missing alot
    
    * changing uaa creds for dev and messing with uaa in script
    
    * making it work with string
    
    * fixing typos
    
    * refactor audit events to S3 script and add tests
    
    * move requirements file
    
    * change pipeline job order
    
    * add pipeline job to run unit tests
    
    * make run-tests job part of group
    
    * update pipeline job order
    
    * fix typo
    
    * fix task file path
    
    * block dev deploy on run-tests job
    
    * fix invalid file name references to task file
    
    * add unit tests for cases of no org and space on audit events
    
    * update audit event uploader script to handle case where org or space no longer exists
    
    * refactor audit events uploader to handle case where org or space does not exist
    
    * add audit event uploader tests for case of requests for org or space returning 404
    
    * simplify logic
    
    * Update ci/upload_audit_events_s3.py
    
    Co-authored-by: Ben Berry <bengerman@gmail.com>
    Signed-off-by: Mark Boyd <markdavidboyd@gmail.com>
    
    * Update ci/upload_audit_events_s3.py
    
    Co-authored-by: Ben Berry <bengerman@gmail.com>
    Signed-off-by: Mark Boyd <markdavidboyd@gmail.com>
    
    * fix case of audit event with no space
    
    * fix Content-Type
    
    ---------
    
    Signed-off-by: Mark Boyd <markdavidboyd@gmail.com>
    Co-authored-by: Mark Boyd <mark.boyd@gsa.gov>
    Co-authored-by: Ben Berry <bengerman@gmail.com>

---
+++ tests/test_audit_logs_s3.py
@@ -0,0 +1,168 @@
+import pytest
+import uuid
+import requests_mock
+import json
+
+from datetime import datetime
+from ci.upload_audit_events_s3 import AuditEventsS3Uploader
+
+
+@pytest.fixture
+def audit_event():
+    return {
+        "guid": str(uuid.uuid4()),
+        "created_at": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "updated_at": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "type": "audit.service_instance.show",
+        "actor": {"guid": str(uuid.uuid4()), "type": "user", "name": "fake-user"},
+        "target": {
+            "guid": str(uuid.uuid4()),
+            "type": "service_instance",
+            "name": "fake-service",
+        },
+        "data": {"request": None},
+        "space": {"guid": str(uuid.uuid4())},
+        "organization": {"guid": str(uuid.uuid4())},
+        "links": {"self": {"href": "fake-url"}},
+    }
+
+
+@pytest.fixture
+def fake_requests(audit_event):
+    with requests_mock.Mocker(real_http=False) as m:
+        m.post(
+            "http://uaa.localhost/oauth/token",
+            text=json.dumps(dict(access_token="fake-token")),
+        )
+
+        org_guid = audit_event["organization"]["guid"]
+        m.get(
+            f"http://cf.localhost/v3/organizations/{org_guid}",
+            text=json.dumps(dict(name="fake-org")),
+        )
+
+        space_guid = audit_event["space"]["guid"]
+        m.get(
+            f"http://cf.localhost/v3/spaces/{space_guid}",
+            text=json.dumps(dict(name="fake-space")),
+        )
+        yield m
+
+
+def test_transform_audit_event(fake_requests, audit_event):
+    audit_events_s3_uploader = AuditEventsS3Uploader()
+    transformed_record = audit_events_s3_uploader.transform_audit_event(audit_event)
+
+    assert transformed_record == {
+        "guid": audit_event["guid"],
+        "created_at": audit_event["created_at"],
+        "updated_at": audit_event["updated_at"],
+        "type": audit_event["type"],
+        "actor": audit_event["actor"],
+        "target": audit_event["target"],
+        "data": audit_event["data"],
+        "space": audit_event["space"],
+        "organization": audit_event["organization"],
+        "organization_name": "fake-org",
+        "space_name": "fake-space",
+    }
+
+
+def test_transform_audit_event_no_space(fake_requests, audit_event):
+    audit_event["space"] = None
+
+    audit_events_s3_uploader = AuditEventsS3Uploader()
+    transformed_record = audit_events_s3_uploader.transform_audit_event(audit_event)
+
+    assert transformed_record == {
+        "guid": audit_event["guid"],
+        "created_at": audit_event["created_at"],
+        "updated_at": audit_event["updated_at"],
+        "type": audit_event["type"],
+        "actor": audit_event["actor"],
+        "target": audit_event["target"],
+        "data": audit_event["data"],
+        "space": audit_event["space"],
+        "organization": audit_event["organization"],
+        "organization_name": "fake-org",
+    }
+
+    assert len(fake_requests.request_history) == 2
+    org_guid = audit_event["organization"]["guid"]
+    assert fake_requests.request_history[1].path == f"/v3/organizations/{org_guid}"
+
+
+def test_transform_audit_event_no_organization(fake_requests, audit_event):
+    audit_event["organization"] = None
+
+    audit_events_s3_uploader = AuditEventsS3Uploader()
+    transformed_record = audit_events_s3_uploader.transform_audit_event(audit_event)
+
+    assert transformed_record == {
+        "guid": audit_event["guid"],
+        "created_at": audit_event["created_at"],
+        "updated_at": audit_event["updated_at"],
+        "type": audit_event["type"],
+        "actor": audit_event["actor"],
+        "target": audit_event["target"],
+        "data": audit_event["data"],
+        "space": audit_event["space"],
+        "organization": audit_event["organization"],
+        "space_name": "fake-space",
+    }
+
+    assert len(fake_requests.request_history) == 2
+    space_guid = audit_event["space"]["guid"]
+    assert fake_requests.request_history[1].path == f"/v3/spaces/{space_guid}"
+
+
+def test_transform_audit_event_organization_does_not_exist(fake_requests, audit_event):
+    org_guid = audit_event["organization"]["guid"]
+    fake_requests.get(
+        f"http://cf.localhost/v3/organizations/{org_guid}",
+        status_code=404,
+    )
+
+    audit_events_s3_uploader = AuditEventsS3Uploader()
+    transformed_record = audit_events_s3_uploader.transform_audit_event(audit_event)
+
+    assert transformed_record == {
+        "guid": audit_event["guid"],
+        "created_at": audit_event["created_at"],
+        "updated_at": audit_event["updated_at"],
+        "type": audit_event["type"],
+        "actor": audit_event["actor"],
+        "target": audit_event["target"],
+        "data": audit_event["data"],
+        "space": audit_event["space"],
+        "organization": audit_event["organization"],
+        "space_name": "fake-space",
+    }
+
+    assert len(fake_requests.request_history) == 3
+
+
+def test_transform_audit_event_space_does_not_exist(fake_requests, audit_event):
+    space_guid = audit_event["space"]["guid"]
+    fake_requests.get(
+        f"http://cf.localhost/v3/spaces/{space_guid}",
+        status_code=404,
+    )
+
+    audit_events_s3_uploader = AuditEventsS3Uploader()
+    transformed_record = audit_events_s3_uploader.transform_audit_event(audit_event)
+
+    assert transformed_record == {
+        "guid": audit_event["guid"],
+        "created_at": audit_event["created_at"],
+        "updated_at": audit_event["updated_at"],
+        "type": audit_event["type"],
+        "actor": audit_event["actor"],
+        "target": audit_event["target"],
+        "data": audit_event["data"],
+        "space": audit_event["space"],
+        "organization": audit_event["organization"],
+        "organization_name": "fake-org",
+    }
+
+    assert len(fake_requests.request_history) == 3
