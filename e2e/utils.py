d76b29338da976889232d19af9c4337fe948fcd2
Mark Boyd <mark.boyd@gsa.gov>
2026-01-09 15:54:48 -0500

    make e2e test logic expanding table rows conditional on whether button to update table size is visible

--- e2e/utils.py
+++ e2e/utils.py
@@ -147,9 +147,11 @@ def update_rows_per_table(page, rows_option="50 rows"):
     rows_per_page_button = page.get_by_role(
         "button", name=re.compile(r"^Rows per page: [0-9]+$")
     )
-    rows_per_page_button.wait_for()
-    rows_per_page_button.click()
 
-    fifty_rows_button = page.get_by_role("button", name=rows_option, exact=True)
-    fifty_rows_button.wait_for()
-    fifty_rows_button.click()
+    if rows_per_page_button.is_visible():
+        rows_per_page_button.wait_for()
+        rows_per_page_button.click()
+
+        fifty_rows_button = page.get_by_role("button", name=rows_option, exact=True)
+        fifty_rows_button.wait_for()
+        fifty_rows_button.click()
