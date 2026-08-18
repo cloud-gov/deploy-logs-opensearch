a0628a169079433c92663417945861237c141e4d
Jason <jason.gambino@gsa.gov>
2026-08-18 17:20:55 -0400

    removing more e2e

--- scripts/e2e.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python -m venv venv
-# shellcheck disable=SC1091
-source venv/bin/activate
-pip install -r requirements-test.txt
-playwright install firefox
-python -m pytest e2e --browser firefox --tracing retain-on-failure "$@"
