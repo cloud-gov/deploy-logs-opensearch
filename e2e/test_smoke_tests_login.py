647b80b05f9e34c49eaacf6bff47299455025c6e
Jason <jason.gambino@gsa.gov>
2026-08-19 13:45:40 -0400

    testing

--- e2e/test_smoke_tests_login.py
+++ e2e/test_smoke_tests_login.py
@@ -5,7 +5,7 @@ import logging
 from playwright.sync_api import expect
 from urllib.parse import urljoin
 
-from .utils import log_in
+from .utils import log_in, switch_tenants
 from . import AUTH_PROXY_URL
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,3 +14,4 @@ logging.basicConfig(level=logging.DEBUG)
 def test_user_login(user_1, page):
     log_in(user_1, page, AUTH_PROXY_URL)
     expect(page).to_have_url(re.compile(f"{urljoin(AUTH_PROXY_URL, 'app/home')}.*"))
+    switch_tenants(page, tenant="cloud-gov")
